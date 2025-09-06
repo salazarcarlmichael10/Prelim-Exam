411b240afc33c994e3fc7e512438e10eda5cf938
salazarcarlmichael10 <salazarcarlmichael10@gmail.com>
2025-09-06 17:37:02 +0800

    Add files via upload

--- GameApp/Libraries/Potion.cs
+++ GameApp/Libraries/Potion.cs
@@ -1,1 +1,22 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using GameApp.Libraries.Abstract;
+namespace GameApp.Libraries.Interface
+{
 
+    public class Potion : GameItem
+    {
+        private int HealAmount;
+
+        // Constructor - creates a Potion with a Name and HealingAmount value
+        public Potion(string name, int healamount) : base(name)
+        {
+            this.HealAmount = healamount;
+
+        }
+        // Shows us what happens when you use the Potion
+        public override void UseItem()
+        {
+            Console.WriteLine("You drank " + this.Name + ". " + "Restored " + this.HealAmount + "HP");
+        }
+    }
+}
--- GameApp/Libraries/Shield.cs
+++ GameApp/Libraries/Shield.cs
@@ -1,1 +1,24 @@
+﻿using GameApp.Libraries.Abstract;
+namespace GameApp.Libraries.Interface
+{
+    public class Shield : GameItem
+    {
+        private int Defense;
 
+        // Constructor - creates a Shield with a Name and Defense value
+        public Shield(string name, int defense) : base(name)
+        {
+            this.Defense = defense;
+        }
+
+
+
+        // Show us what happens when you use the Shield
+        public override void UseItem()
+        {
+            Console.WriteLine("You equipped " + this.Name + ". " + "Defense Increase by " + this.Defense);
+        }
+    }
+
+
+}
