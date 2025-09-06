1eb36c3ce250ed68dbf2c9ca6149f78cde2b73d1
salazarcarlmichael10 <salazarcarlmichael10@gmail.com>
2025-09-06 17:38:27 +0800

    Add files via upload

--- GameApp/Libraries/Interface/IGameItem.cs
+++ GameApp/Libraries/Interface/IGameItem.cs
@@ -1,1 +1,14 @@
+﻿
 
+
+namespace GameApp.Libraries.Interface
+{
+
+    // this is an interface,and allows any classes to use the UsedItem method
+    public interface IGameItem
+    {
+        public void UseItem();
+
+    }
+
+}
