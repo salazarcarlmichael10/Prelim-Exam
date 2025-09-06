7b23c11000247b3ed7e56be5b79a2deee12e0694
salazarcarlmichael10 <salazarcarlmichael10@gmail.com>
2025-09-06 17:36:27 +0800

    Add files via upload

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -1,1 +1,39 @@
+﻿
+using GameApp.Libraries.Interface;
 
+
+namespace GameApp
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //used to create the object for shield
+            Shield newshield = new Shield("Iron Shield", 80);
+            //usee to create the object for potion
+            Potion newpotion = new Potion("Healing Potion", 50);
+
+            newshield.ShowInfo();
+            newshield.UseItem();
+            newpotion.ShowInfo();
+            newpotion.UseItem();
+
+
+
+            Console.WriteLine("----------------------------------------");
+
+
+            Console.Write("Enter Birth Place: ");
+            string birthPlace = Console.ReadLine();
+            Console.Write("Enter Nationality: ");
+            string nation = Console.ReadLine();
+
+            Console.WriteLine("You are born in " + birthPlace + " and your nationality is " + nation);
+
+            //made by Salazar and Pillar
+
+        }
+
+    }
+}
