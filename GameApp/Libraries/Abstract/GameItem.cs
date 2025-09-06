b23ca4bbe2efd94e1d816087faf6f9685d7855c7
salazarcarlmichael10 <salazarcarlmichael10@gmail.com>
2025-09-06 17:37:45 +0800

    Add files via upload

--- GameApp/Libraries/Abstract/GameItem.cs
+++ GameApp/Libraries/Abstract/GameItem.cs
@@ -1,1 +1,30 @@
+﻿
 
+using GameApp.Libraries.Interface;
+namespace GameApp.Libraries.Abstract
+{
+
+    // an abstract class which allows other class to inherit it
+    public abstract class GameItem
+    {
+        // a variable to store the item’s name
+        public string Name;
+
+
+
+        // a constructor - runs when you make a new item
+        public GameItem(string itemName)
+        {
+            this.Name = itemName;
+        }
+
+        // it is a method which can be called to show the item’s info.
+        public void ShowInfo()
+        {
+            Console.WriteLine("Item: " + this.Name);
+        }
+
+        public abstract void UseItem();
+
+    }
+}
